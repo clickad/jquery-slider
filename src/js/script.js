a05335c60a284273ae1b8734f08dcef30efad426
clickad <adnan.damadzic@gmail.com>
2017-08-09 13:41:54 +0430

    change widht by adjusting the json

--- src/js/script.js
+++ src/js/script.js
@@ -7,19 +7,34 @@
  $(document).ready(function(){
  
   $.when(getData()).then(function (data) {
-
+   
+     var slider = $('.slider');
      var sliderView = $('.slider__wrapper');
      var sliderTracker = $('.slider__tracker');
      var _count = 1; 
-     var fadeTime = 1500; // Change this to adjust speed
-     var sliders = data;
+     var fadeTime = data.speed; 
+     var width = data.width;
+     var height = data.height;
+     var sliders = data.sliders;
 
+    //set height and width from json
+
+    if(width > 100) {width = '100'};
+    if(height > $(window).height()) {height = $(window).height()};
+    slider.width(width + '%');
+    slider.height(height + 'px');
 
     // For each object in json slider append to html slider wrapper tag
 
      for(var i = 0; i < sliders.length; i++){
-       sliderView.append('<div class = "slider__img" style = "background-image:url(' + sliders[i].src + ')"><div class ="slider__box"><div class = "slider__info"><h2 class = "slider__title"> ' + sliders[i].title + ' </h2><div class ="slider__text--wrapper"><h5 class = "slider__text">' + sliders[i].text + '</h5></div><button class = "slider__link">' + sliders[i].btn +'</button></div></div></div>');
-       sliderTracker.append('<span class = "trackers"></span>');
+    sliderView.append('<div class = "slider__img" style = "background-image:url(' + sliders[i].src + ')"><div class ="slider__box"><div 
+
+class = "slider__info"><h2 class = "slider__title"> ' + sliders[i].title + ' </h2><div class ="slider__text--wrapper"><h5 class = 
+
+"slider__text">' + sliders[i].text + '</h5></div><button class = "slider__link">' + sliders[i].btn +'</button></div></div></div>');
+
+     sliderTracker.append('<span class = "trackers"></span>');
+
     }
 
     // Show the first slider
